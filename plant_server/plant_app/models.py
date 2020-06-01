71ab9e72f699ee89aab99323c6600eccf7240501
yuy8313 <yuy8313@naver.com>
2020-06-01 15:24:22 +0900

    장고서버 서버예제 연습

--- plant_server/plant_app/models.py
+++ plant_server/plant_app/models.py
@@ -2,5 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+class Test(models.Model):
+    test = models.CarFileId(max_length=10)
 
-
+    def __str__(self):
+        return self.test
--- plant_server/plant_app/views.py
+++ plant_server/plant_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+
+from .models import Test
+from .serializers import TestSerializer
+
+class TestViewSet(viewsets.ModelViewSet):
+    queryset = Test.objects.all()
+    serializer_class = TestSerializer
